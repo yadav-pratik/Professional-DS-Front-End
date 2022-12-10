081768bbe9f9a6b8bdf5ab2c09a7b03089746adf
pratik <yadavpratik8806@gmail.com>
2022-12-10 20:50:24 +0530

    created Home and NavBar components and added links to NavBar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from "react"
+import NavBar from "./components/NavBar";
 
 const App = (props) => {
   return (
     <div>
       <h1>Pro-Doorstep Services</h1>
+      <NavBar />
     </div>
   );
 }
---
+++ src/components/NavBar.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const NavBar = (props) => {
+  return (
+    <div>
+        { !localStorage.getItem('token') ? (
+                <div>
+                    <Link to="/user/services"><span>Services</span></Link>
+                    <Link to="/user/account"><span>Account</span></Link>
+                    <Link to="/user/settings"><span>Settings</span></Link>
+                </div>
+            ) : (
+                <div>
+                    <Link to="/home"><span>Home</span></Link>
+                    <Link to="/user/register"><span>Register</span></Link>
+                    <Link to="/user/login"><span>Login</span></Link>
+                </div>
+            )
+        }
+    </div>
+  )
+}
+
+export default NavBar
