dbaac8b9a9d4e4ea97f9b973577e61a599135a01
pratik <yadavpratik8806@gmail.com>
2022-12-15 00:08:24 +0530

    added usedispatch and useSeletor functions inside Addresses

--- src/components/Addresses.js
+++ src/components/Addresses.js
@@ -1,9 +1,21 @@
 import React from 'react'
+import { useSelector, useDispatch} from 'react-redux'
 
 const Addresses = (props) => {
+
+  const dispatch = useDispatch()
+
+  const { addresses } = useSelector((state)=> {
+    return state
+  })
+
   return (
     <div>
-        <h2>Your Addresses</h2>
+        <h2>Your Addresses - {addresses.length}</h2>
+        {addresses.map( address => {
+          return <div>
+          </div>
+        })}
     </div>
   )
 }
