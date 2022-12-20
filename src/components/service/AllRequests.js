7cc61196395091457e451650264d61da1737f4f1
pratik <yadavpratik8806@gmail.com>
2022-12-21 00:03:53 +0530

    added allRequests component

---
+++ src/components/service/AllRequests.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+const AllRequests = (props) => {
+    return (
+        <div>
+            <h3>Your Service Requests</h3>
+        </div>
+    )
+}
+
+export default AllRequests
--- src/components/service/Services.js
+++ src/components/service/Services.js
@@ -2,15 +2,18 @@ import React from "react"
 import { Link, Route } from "react-router-dom"
 
 import AddRequest from "./AddRequest"
+import AllRequests from "./AllRequests"
 
 const Services = (props) => {
     return (
         <div>
             <h2>Services Page</h2>
-            <span><Link to="/user/services/add-request">Add Service Request</Link></span>
-            <span><Link to="/user/services/past-requests">Your Past Requests</Link></span>
+            <span><Link to="/user/services/add-request">Add New Request</Link></span>
+            <span><Link to="/user/services/all-requests">Your Requests</Link></span>
 
             <Route path="/user/services/add-request" component={AddRequest} />
+            <Route path="/user/services/all-requests" component={AllRequests} /> 
+
         </div>
     )
 }
