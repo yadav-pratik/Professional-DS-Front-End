c14b9803374cd5459f18ca28da34926d9a28736d
pratik <yadavpratik8806@gmail.com>
2022-12-22 18:27:18 +0530

    AllRequests will render when service page is clicked

--- src/components/service/Services.js
+++ src/components/service/Services.js
@@ -1,18 +1,32 @@
-import React from "react"
+import React, { useEffect } from "react"
 import { Link, Route } from "react-router-dom"
+import { useSelector, useDispatch } from "react-redux"
 
 import AddRequest from "./AddRequest"
 import AllRequests from "./AllRequests"
 
+import { startGetAddresses } from "../../actions/addressesAction"
+import { startGetUser } from "../../actions/userActions"
+
 const Services = (props) => {
+
+    const { user, addresses } = useSelector((state) => {
+        return state
+    })
+
+    const dispatch = useDispatch()
+    useEffect(()=>{
+        dispatch(startGetUser())
+        dispatch(startGetAddresses())
+    },[])
     return (
         <div>
             <h2>Services Page</h2>
             <span><Link to="/user/services/add-request">Add New Request</Link></span>
-            <span><Link to="/user/services/all-requests">Your Requests</Link></span>
+            <span><Link to="/user/services">Your Requests</Link></span>
 
             <Route path="/user/services/add-request" component={AddRequest} />
-            <Route path="/user/services/all-requests" component={AllRequests} /> 
+            <Route path="/user/services" component={AllRequests} exact/> 
 
         </div>
     )
