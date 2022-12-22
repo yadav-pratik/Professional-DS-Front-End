c6bdd47759bc4e32818638c4317670470581bbef
pratik <yadavpratik8806@gmail.com>
2022-12-22 23:12:18 +0530

    added by default selected address will be the default address

--- src/components/address/AddressLine.js
+++ src/components/address/AddressLine.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useEffect } from "react"
 import { useDispatch } from "react-redux"
 
 
@@ -17,6 +17,12 @@ const AddressLine = (props) => {
         addressChange, 
         selectedAddress
     } = props
+
+    useEffect(()=>{
+        if(defaultAddress === true && selectedAddress.length === 0){
+            addressChange(_id)
+        }
+    },[])
 
     const dispatch = useDispatch()
 
