5ba579886413bf8b3153caf5b66087ecb8210c08
pratik <yadavpratik8806@gmail.com>
2022-12-24 20:47:46 +0530

    added dropdown inside AllRequest

--- src/components/service/AllRequests.js
+++ src/components/service/AllRequests.js
@@ -1,9 +1,37 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 
 const AllRequests = (props) => {
+    const [status, setStatus] = useState('all')
+
+    const statusType = ['added', 'alloted', 'completed']
+
+    useEffect(()=>{
+        
+    }, [status])
+
+    const handleStatusChange = (e) => {
+        setStatus(e.target.value)
+    }
     return (
         <div>
             <h3>Your Service Requests</h3>
+            <form>
+                <label>Filter by Status</label>
+                <select
+                    value={status}
+                    onChange={handleStatusChange}
+                >
+                    <option value='all'>All</option>
+                    {statusType.map((status, i)=>{
+                        return <option
+                            key={i}
+                            value={status}
+                        >
+                            {status.charAt(0).toUpperCase() + status.slice(1)}
+                        </option>
+                    })}
+                </select>
+            </form>
         </div>
     )
 }
