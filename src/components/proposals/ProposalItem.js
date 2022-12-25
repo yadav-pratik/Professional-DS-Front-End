8755b67486b285c826f18b345f7b946ef2d61b60
pratik <yadavpratik8806@gmail.com>
2022-12-26 01:51:53 +0530

    proposalItem added border and name of professional

--- src/components/proposals/ProposalItem.js
+++ src/components/proposals/ProposalItem.js
@@ -1,12 +1,36 @@
 import React, { useState } from "react"
+import { useSelector } from "react-redux"
 
 const ProposalItem = (props) => {
-    const {_id, amount, date, professional} = props
+    const {_id, amount, date, professional, status} = props
+
+    const user = useSelector((state) => {
+        return state.user
+    })
+
     return (
-        <div>
-            <p>Proposed by - {professional}</p>
+        <div style={{borderWidth : '2px', borderColor : 'black', borderStyle : 'solid'}}>
+            <p>Proposed by - {professional.name}</p>
             <b>Amount - Rs. {amount}</b>
             <span>Proposed Date - {date.slice(0,10)}</span>
+            {user.role === 'customer' ? (
+                    <div>
+                        {status === 'pending' ? (
+                                <p>{'Status - '} 
+                                    <button>Accept</button>
+                                    <button>Reject</button>
+                                </p>
+                            ) : (
+                                <p>Status - {status.charAt(0).toUpperCase() + status.slice(1)}</p>
+                            )
+                        } 
+                    </div>
+                ) : (
+                    <div>
+                        control for professional
+                    </div>
+                )
+            }
         </div>
     )
 }
